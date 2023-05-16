761f40cdeb1bbd15919e49b407ccc390ec607b79
Maurício Marques de Oliveira <mauriciommo@gmail.com>
2023-05-16 10:11:49 -0300

    Curso

---
+++ ex002/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $canal = "Curso em Vídeo \u{1F499}";
+        CONST CANAL = "Curso em Vídeo \u{1F499}";
+
+        echo "Eu adoro o " . $canal;
+    
+    
+    ?>
+</body>
+</html>
